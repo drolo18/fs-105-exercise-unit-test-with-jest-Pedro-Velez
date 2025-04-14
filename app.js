a6b19c372a32c5306dd9f7c7eab10ad132151e7e
Pedro Vélez <pedrolo.pv@gmail.com>
2025-04-14 19:18:48 +0000

    exercise 11

--- app.js
+++ app.js
@@ -2,4 +2,39 @@ const sum =(a,b) =>{
     return a + b 
 }
 console.log(sum(7,3))
-module.exports = {sum};
+
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    
+    let valueInEuro = valueInDollar / oneEuroIs.USD
+    
+    let valueInYen = valueInEuro * oneEuroIs.JPY
+    
+    return valueInYen
+}
+
+const fromYenToPound = function(valueInYen) {
+    
+    let valueInEuro = valueInYen / oneEuroIs.JPY
+    
+    let valueInPound = valueInEuro * oneEuroIs.GBP
+    
+    return valueInPound
+}
+
+
+module.exports = {sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
