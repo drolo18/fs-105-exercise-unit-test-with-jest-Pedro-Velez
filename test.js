277c8b7035d5e341fca84131536250fb297fdf2b
Pedro Vélez <pedrolo.pv@gmail.com>
2025-04-16 18:00:16 +0000

    test fail bug

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-const {sum} = require('./app.js');
+const {sum , fromDollarToYen, fromYenToPound, fromEuroToDollar} = require('./app.js');
 
 test('adds 14 + 9 to equal 23', () =>{
     let total = sum (14,9);
     expect(total).toBe(23);
 })
+describe
 test("One euro should be 1.07 dollars", function() {
-    // Importo la funcion desde app.js
-    const { fromEuroToDollar } = require('./app.js');
 
     // Uso la función como debe ser usada
     const dollars = fromEuroToDollar(3.5);
@@ -15,5 +14,23 @@ test("One euro should be 1.07 dollars", function() {
     const expected = 3.5 * 1.07;
 
     // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(fromEuroToDollar(dollars)).toBe(expected); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+
+test("One dollar should be 146.26 yens", function() {
+
+    const yens = fromDollarToYen(1);
+ 
+    const expected1 = 146.27;
+
+    expect(fromDollarToYen(yens)).toBe(expected1); 
+})
+
+test("One yen should be 0.0056 pounds", function() {
+
+    const pounds = fromYenToPound(1);
+
+    const expected2 = 0.0056;
+
+    expect(fromYenToPound(pounds)).toBe(expected2); 
 })
