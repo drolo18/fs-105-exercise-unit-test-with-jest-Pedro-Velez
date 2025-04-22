ef84920f484f6154f376359403cc7c0ba5b96cf7
Pedro Vélez <pedrolo.pv@gmail.com>
2025-04-22 15:08:17 +0000

    bug fixed

--- test.js
+++ test.js
@@ -12,8 +12,8 @@ describe('Conversión de Euros a Dólares', () => {
         // Uso la función como debe ser usada
         const dollars = fromEuroToDollar(3.5);
 
-        // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-        const expected = 3.5 * 1.07;
+        // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.75)
+        const expected = 3.75;
 
         // Hago mi comparación (la prueba)
         expect(dollars).toBe(expected);
@@ -23,15 +23,15 @@ describe('Conversión de Euros a Dólares', () => {
 describe('Conversión de Dólares a Yenes', () => {
     test("One dollar should be 146.26 yens", function () {
         const yens = fromDollarToYen(1);
-        const expected1 = 146.26168224299064;
+        const expected1 = 146.26;
         expect(yens).toBe(expected1);
     })
 })
 
 describe('Conversión de Yenes a Libras', () => {
-    test("One yen should be 0.0056 pounds", function () {
-        const pounds = fromYenToPound(1);
-        const expected2 = 0.0055591054313099035;
+    test("two yen should be 0.01 pounds", function () {
+        const pounds = fromYenToPound(2);
+        const expected2 = 0.01;
         expect(pounds).toBe(expected2);
     })
 })
